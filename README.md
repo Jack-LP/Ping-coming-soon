31b4ad0ccdcbb41f29bfc8fc6bebcffd98fb5ae8
Jack Paget <jackpaget1@gmail.com>
2022-04-02 20:33:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ping-coming-soon
+  This is an example page produced for Frontend Mentor 
---
+++ app.js
@@ -0,0 +1,17 @@
+function validation() {
+  let form = document.getElementById('form');
+  let email = document.getElementById('email-input').value;
+  let text = document.getElementById('error-msg');
+  let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.match(pattern)) {
+    emailInput.classList.remove('invalid');
+    text.innerHTML = '';
+  } else {
+    emailInput.classList.add('invalid');
+    text.innerHTML = 'Please provide a valid email address';
+  }
+}
+
+const emailInput = document.getElementById('email-input');
+emailInput.addEventListener('keydown', validation);
